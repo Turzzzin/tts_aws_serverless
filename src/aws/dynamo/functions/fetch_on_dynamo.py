910b42559e8e7df53493a811919cc658e4b088d1
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-10-03 15:17:04 -0300

    feat: add fetch_on_dynamo function

---
+++ src/aws/dynamo/functions/fetch_on_dynamo.py
@@ -0,0 +1,16 @@
+from src.aws.dynamo.dynamo_client import dynamo
+from dotenv import load_dotenv
+from os import getenv as env
+
+load_dotenv()
+
+def fetch_hash_on_dynamo():
+    try:
+        key = {
+            "hash": {"S": hash}
+        }
+
+        item = dynamo.get_item(TableName=env('dynamo_table'), Key=key)
+    except Exception as err:
+        return f'error: {str(err)}'
+        # raise err
